b684477d0fbd13e9d59f01df7c45d61413022b0f
MegDharap <134129278+MegDharap@users.noreply.github.com>
2023-11-23 23:26:41 +0530

    Keyword Counter.py

--- Hello.py
+++ Hello.py
@@ -1,51 +1,56 @@
-# Copyright (c) Streamlit Inc. (2018-2022) Snowflake Inc. (2022)
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-
 import streamlit as st
-from streamlit.logger import get_logger
+import re
+import pandas as pd
 
-LOGGER = get_logger(__name__)
+def count_keyword_occurrences(text, keyword_list):
+    # Lowercase the text and the keywords for consistency
+    text = text.lower()
+    keyword_list = [keyword.lower() for keyword in keyword_list]
 
+    # Count occurrences
+    occurrences = {keyword: len(re.findall(r"\b" + re.escape(keyword) + r"\b", text)) for keyword in keyword_list}
+    return occurrences
 
-def run():
-    st.set_page_config(
-        page_title="Hello",
-        page_icon="👋",
-    )
+# Function to convert DataFrame to markdown
+def df_to_markdown(df):
+    markdown = ' | ' + ' | '.join(df.columns) + ' |\n' + ' | ' + ' | '.join(['---'] * len(df.columns)) + ' |\n'
+    for index, row in df.iterrows():
+        markdown += ' | ' + ' | '.join(str(cell) for cell in row) + ' |\n'
+    return markdown
 
-    st.write("# Welcome to Streamlit! 👋")
+# Title for the app
+st.title('Pepper Content \n'
+         '### Keyword Frequency Counter')
 
-    st.sidebar.success("Select a demo above.")
+# Instructions
+st.write('Please input your entire article and all the target keywords. '
+         'Then hit Submit to analyse the keyword frequency.')
 
-    st.markdown(
-        """
-        Streamlit is an open-source app framework built specifically for
-        Machine Learning and Data Science projects.
-        **👈 Select a demo from the sidebar** to see some examples
-        of what Streamlit can do!
-        ### Want to learn more?
-        - Check out [streamlit.io](https://streamlit.io)
-        - Jump into our [documentation](https://docs.streamlit.io)
-        - Ask a question in our [community
-          forums](https://discuss.streamlit.io)
-        ### See more complex demos
-        - Use a neural net to [analyze the Udacity Self-driving Car Image
-          Dataset](https://github.com/streamlit/demo-self-driving)
-        - Explore a [New York City rideshare dataset](https://github.com/streamlit/demo-uber-nyc-pickups)
-    """
-    )
+# Creating a text area for the user to input a large piece of text
+text_input = st.text_area('Paste your article here:', height=300)
 
+# Creating a text input for the user to enter a list of key phrases separated by commas
+keyword_input = st.text_area('Enter the full list of keywords, separated by commas:', height=150)
 
-if __name__ == "__main__":
-    run()
+# Submit button
+submit_button = st.button('Submit')
+
+if submit_button:
+    # Split keywords by commas and strip whitespace
+    keyword_list = [keyword.strip() for keyword in keyword_input.split(',')]
+
+    if text_input and keyword_list:
+        # Count occurrences
+        occurrences = count_keyword_occurrences(text_input, keyword_list)
+
+        # Convert the dictionary into a pandas DataFrame
+        df = pd.DataFrame(list(occurrences.items()), columns=['Keyword', 'Frequency'])
+
+        # Convert the DataFrame to markdown
+        markdown_table = df_to_markdown(df)
+
+        # Display results in a Markdown-formatted table
+        st.write('Frequency of each key phrase in Markdown format:')
+        st.markdown(markdown_table, unsafe_allow_html=True)
+    else:
+        st.error('Please make sure you have entered the text and keywords.')
